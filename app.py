aaa28d7b28eab4c50edafb1e36bbe9dccf65504b
fnnetwork <fnnetwork272@gmail.com>
2025-01-23 23:54:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,11 @@ def thumbnail(episodeId):
     data = get_postman_urls(link)
     return {"thumb": data[1]}
 
-if __name__ == "__app__":
+@app.route("/")
+def home():
+    return "Hello, Heroku!"
+
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0",
  port=port)
